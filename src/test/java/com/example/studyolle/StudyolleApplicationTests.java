1d4d5785835a52f3cd99d4dcc0068616a234880f
ssunaa <seonflower91@gmail.com>
2020-06-14 15:10:26 +0900

    init commit

---
+++ src/test/java/com/example/studyolle/StudyolleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyolle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyolleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
