6c3f9e860e0c2f153910d0b566816c3cfb9d2c9d
ssunaa <seonflower91@gmail.com>
2020-06-23 23:20:17 +0900

    6/23 로그인로그아웃 테스트코드 작성, 로그인유지(쿠키 탈취 방지)

--- src/main/java/com/studyolle/config/SecurityConfig.java
+++ src/main/java/com/studyolle/config/SecurityConfig.java
@@ -1,23 +1,34 @@
 package com.studyolle.config;
 
+import com.studyolle.account.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity //시큐리티 활성화
+@RequiredArgsConstructor //생성자 자동생성
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AccountService accountService;
+    private final DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .mvcMatchers("/", "/sign-up", "/check-email-token"
+                .mvcMatchers("/", "login", "/sign-up", "/check-email-token"
                         , "/email-login", "/check-mail-login", "/login-link").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
                 .anyRequest().authenticated();
@@ -27,6 +38,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.logout()
                 .logoutSuccessUrl("/");
+
+        http.rememberMe()
+                .userDetailsService(accountService)
+                .tokenRepository(tokenRepository());
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
     }
 
     @Override
