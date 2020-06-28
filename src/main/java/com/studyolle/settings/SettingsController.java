85841d0ba68ac728cf21cd2e3f852cf8aae04f15
ssunaa <seonflower91@gmail.com>
2020-06-29 00:01:12 +0900

    6/28 프로필 수정 테스트코드 작성

--- src/main/java/com/studyolle/settings/SettingsController.java
+++ src/main/java/com/studyolle/settings/SettingsController.java
@@ -18,9 +18,8 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class SettingsController {
 
-    private static final String SETTINGS_PROFILE_VIEW_NAME = "settings/profile";
-
-    private static final String SETTINGS_PROFILE_URL = "/settings/profile";
+    static final String SETTINGS_PROFILE_VIEW_NAME = "settings/profile";
+    static final String SETTINGS_PROFILE_URL = "/settings/profile";
 
     private final AccountService accountService;
 
@@ -38,17 +37,17 @@ public class SettingsController {
         return SETTINGS_PROFILE_VIEW_NAME;
     }
 
-    @PostMapping("/settings/profile")
-    public String updateProfile(@CurrentUser Account account, @Valid @ModelAttribute Profile profile, Errors errors, Model model, RedirectAttributes attribut) {
-
+    @PostMapping(SETTINGS_PROFILE_URL)
+    public String updateProfile(@CurrentUser Account account, @Valid Profile profile, Errors errors,
+                                Model model, RedirectAttributes attributes) {
         if (errors.hasErrors()) {
             model.addAttribute(account);
             return SETTINGS_PROFILE_VIEW_NAME;
         }
 
         accountService.updateProfile(account, profile);
-        attribut.addFlashAttribute("message", "프로필을 수정했습니다.");
-        return "redirect:/profile/" + account.getNickname();
+        attributes.addFlashAttribute("message", "프로필을 수정했습니다.");
+        return "redirect:" + SETTINGS_PROFILE_URL;
     }
 
 }
