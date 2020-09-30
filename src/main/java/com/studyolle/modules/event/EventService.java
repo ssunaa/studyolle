1b426d91371e91981c37b38f679c112dbea85498
ssunaa <seonflower91@gmail.com>
2020-09-30 20:38:34 +0900

    9/30 모임쪽 알림 추가(모임생성, 모임수정, 모임취소, 승인거절시 알림)

--- src/main/java/com/studyolle/modules/event/EventService.java
+++ src/main/java/com/studyolle/modules/event/EventService.java
@@ -1,10 +1,14 @@
 package com.studyolle.modules.event;
 
 import com.studyolle.modules.account.Account;
+import com.studyolle.modules.event.event.EnrollmentAcceptedEvent;
+import com.studyolle.modules.event.event.EnrollmentRejectedEvent;
 import com.studyolle.modules.study.Study;
 import com.studyolle.modules.event.form.EventForm;
+import com.studyolle.modules.study.event.StudyUpdateEvent;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,22 +22,42 @@ public class EventService {
     private final EventRepository eventRepository;
     private final EnrollmentRepository enrollMentRepository;
     private final ModelMapper modelMapper;
+    private final ApplicationEventPublisher eventPublisher;
 
+    /**
+     * 모임 생성
+     * @param event
+     * @param study
+     * @param account
+     * @return
+     */
     public Event createEvent(Event event, Study study, Account account) {
         event.setCreatedBy(account);
         event.setCreatedDateTime(LocalDateTime.now());
         event.setStudy(study);
+        eventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy(),
+                "'" + event.getTitle() + "' 모임을 만들었습니다."));
         return eventRepository.save(event);
     }
 
+    /**
+     * 모임 수정
+     * @param event
+     * @param eventForm
+     */
     public void updateEvent(Event event, EventForm eventForm) {
         modelMapper.map(eventForm, event);
         //TODO 모집 인원을 늘린 선착순 모임의 경우에, 자동으로 추가 인원의 참가 신청을 확정 상태로 변경해야 한다. (나중에 할 일)
+        event.acceptWaitingList();
+        eventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy(),
+                "'" + event.getTitle() + "' 모임 정보를 수정했습니다."));
         event.acceptWaitingList();
     }
 
     public void deleteEvent(Event event) {
         eventRepository.delete(event);
+        eventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy(),
+                "'" + event.getTitle() + "' 모임을 취소했습니다."));
     }
 
     public void newEnrollment(Event event, Account account) {
@@ -59,10 +83,12 @@ public class EventService {
 
     public void acceptEnrollment(Event event, Enrollment enrollment) {
         event.accept(enrollment);
+        eventPublisher.publishEvent(new EnrollmentAcceptedEvent(enrollment));
     }
 
     public void rejectEnrollment(Event event, Enrollment enrollment) {
         event.reject(enrollment);
+        eventPublisher.publishEvent(new EnrollmentRejectedEvent(enrollment));
     }
 
     public void checkInEnrollment(Enrollment enrollment) {
