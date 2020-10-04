05a854871f70e9e16b5d2d0f1981f70f9b7ab0bf
ssunaa <seonflower91@gmail.com>
2020-10-04 14:48:17 +0900

    10/4 패치조인 적용

--- src/main/java/com/studyolle/modules/study/StudyRepositoryExtensionImpl.java
+++ src/main/java/com/studyolle/modules/study/StudyRepositoryExtensionImpl.java
@@ -2,6 +2,9 @@ package com.studyolle.modules.study;
 
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
+import com.studyolle.modules.account.QAccount;
+import com.studyolle.modules.tag.QTag;
+import com.studyolle.modules.zone.QZone;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
@@ -18,7 +21,10 @@ public class StudyRepositoryExtensionImpl extends QuerydslRepositorySupport impl
         JPQLQuery<Study> query = from(study).where(study.published.isTrue()
                 .and(study.title.containsIgnoreCase(keyword))
                 .or(study.tags.any().title.containsIgnoreCase(keyword))
-                .or(study.zones.any().localNameOfCity.containsIgnoreCase(keyword)));
+                .or(study.zones.any().localNameOfCity.containsIgnoreCase(keyword)))
+                .leftJoin(study.tags, QTag.tag).fetchJoin()
+                .leftJoin(study.zones, QZone.zone).fetchJoin()
+                .leftJoin(study.members, QAccount.account).fetchJoin();
         return query.fetch();
     }
 }
